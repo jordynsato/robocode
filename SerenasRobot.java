6f05f57454f2ffddc931f15f7e68db7adfefca67
Serena Ing <sming@cpp.edu>
2018-03-03 23:00:48 -0800

    SerenaBot update

--- SerenasRobot.java
+++ SerenasRobot.java
@@ -1,5 +1,6 @@
 package smsi;
 import robocode.*;
+import java.awt.*;
 //import java.awt.Color;
 
 // API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
@@ -13,35 +14,48 @@ public class SerenasRobot extends AdvancedRobot
 	 * run: SerenasRobot's default behavior
 	 * trying to create oscillating movement behavior
 	 */
-	private double maxWidth, maxHeight;
-	private double currentX, currentY, distance;
-	private int wallPadding = 50;
+	private double distanceX, distanceY;
+	private double direction; //getHeading() to find direction bot is facing
 	boolean ifRobotThere; //if there is a robot there
 
 	public void run() {
 		// Initialization of the robot should be put here
 		// After trying out your robot, try uncommenting the import at the top,
 		// and the next line:
-		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+		setBulletColor(Color.red);
 		// Robot main loop
 		//gets a maximum distance robot should move based on the width and height of the battlefield
 			
-			distance = Math.max(getBattleFieldWidth(), getBattleFieldHeight());
+			distanceX = getBattleFieldWidth()-5;
+			distanceY = getBattleFieldWidth()-5;
 			ifRobotThere=false;
 			turnLeft(getHeading() %90); // moving towards a wall
-			ahead(distance);
+			ahead(distanceX);
 			ifRobotThere=true;
-			
+			turnGunRight(90);
+			//turnRight(90);
 		
 		while(true) {		
 			ifRobotThere = true;
-			ahead(distance);
+			direction = getHeading();
+			if(getY()<=10||getY()>=getBattleFieldHeight()-10||
+				getX() <= 10 || getX() >= getBattleFieldWidth()-10){
+					turnRight(90);
+					if(direction==0||direction==180){
+					ahead(distanceX);
+					}
+					else{
+					ahead(distanceY);
+					}
+				}
+			else{
+			turnRight(90);	
+			ahead(distanceX);
+			}
+		
 			ifRobotThere=false;
-			turnRight(90);
-			ahead(100);
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
+			//turnRight(90);
+			
 			
 			
 		}//end while
